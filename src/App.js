1828473f13aeb11b485177e7d8a541d3f0052a59
kensaku.komatsu <kensaku.komatsu@gmail.com>
2019-06-19 16:32:44 -0700

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Example from './components/example';
 import './App.css';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Example />
       </header>
     </div>
   );
---
+++ src/components/example.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+
+function Example() {
+  const [count, setCount] = useState(0);
+
+  const heandleClick = () => {
+    setCount( count + 1 );
+  }
+
+  useEffect( () => {
+    document.title = `You clicked ${count} times`
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={heandleClick}>
+        Click me
+      </button>
+    </div>
+  )
+}
+
+export default Example
